1b8f0cbefa814637e4a36b910ec7f6ce11566e57
Mark Scheer <mscheer314@gmail.com>
2024-10-16 19:14:39 -0500

    add server and route

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ import express, { Express, Request, Response } from 'express'
 import { connectDB } from './connectDB'
 
 connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hey what up!')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+}) 
