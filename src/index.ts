1c298a5c3e569e639a13649749935a54c0997ebb
Mark Scheer <mscheer314@gmail.com>
2024-10-17 22:17:43 -0500

    fix get call for all tickets

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express, { Express, Request, Response } from 'express'
 import { connectDB } from './connectDB'
-import { Ticket } from './entity/Ticket'
 import { ticketRepository } from './TicketRepository'
 
 connectDB()
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', async (req: Request, res: Response) => {
     // res.send('hey what up!')
-    res.send(ticketRepository.find())
+    const tickets = await ticketRepository.find() 
+    res.json(tickets)
+    // const response = ticketRepository.find().then((data) => {
+        // console.log('in the promise response@!!!!!!')
+        // res.send(data)
+    // })
+    // res.send(ticketRepository.find())
 })
 
 app.listen(3000, () => {
